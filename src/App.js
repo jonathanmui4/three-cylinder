8f51258fcfc97516d41c4577d19d58ed4606231c
Jonathan Mui <jonathanmui4@gmail.com>
2022-04-30 15:06:59 +0800

    Add Shadow

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import Cylinder from './Components/Cylinder';
 import { Canvas } from '@react-three/fiber';
+import { softShadows } from '@react-three/drei';
 import './App.scss';
+
+softShadows();
 
 function App() {
   return (
     <>
-      <Canvas colorManagement camera={{position: [-5, 2, 10], fov: 100}}>
+      <Canvas 
+        shadows
+        colorManagement 
+        camera={{position: [-5, 2, 10], fov: 100}}
+      >
         <ambientLight intensity={0.3} />
-        <Cylinder position={[0, 1, 0]} />
+        <directionalLight
+          castShadow 
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
+        <group>
+          <mesh 
+            receiveShadow 
+            rotation={[-Math.PI/2, 0, 0]} 
+            position={[0, -5, 0]}
+          >
+            <planeBufferGeometry attach="geometry" args={[100, 100]} />
+            <shadowMaterial attach="material" opacity={0.3} />
+          </mesh>
+        </group>
+        <Cylinder position={[0, 1, 0]} args={[2, 2, 6, 40, 4, false]} color='pink' />
       </Canvas>
     </>
   );
--- src/Components/Cylinder.js
+++ src/Components/Cylinder.js
@@ -2,13 +2,13 @@ import { useRef } from "react";
 //import { Cylinder } from '@react-three/drei';
 import { useFrame } from "@react-three/fiber";
 
-export default function Cylinder() {
+export default function Cylinder({ position, args, color }) {
     const mesh = useRef(null);
     useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
     return (
-        <mesh ref={mesh}>
-          <cylinderBufferGeometry attach='geometry' args={[2, 2, 6, 40, 4, false]}/>
-          <meshStandardMaterial attach='material' color='pink'/>
+        <mesh castShadow position={position} ref={mesh}>
+          <cylinderBufferGeometry attach='geometry' args={args}/>
+          <meshStandardMaterial attach='material' color={color}/>
           {/* <Cylinder>
             <meshStandardMaterial attach='material' color='hotpink'/>
           </Cylinder> */}
